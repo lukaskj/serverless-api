8b743d5744dd262b9ef48e61cdda35e1081c1464
Lukas Jacomin <lukasjacomin@gmail.com>
2021-07-08 11:00:20 -0300

    test: env spec

---
+++ tests/unit/util/env.spec.ts
@@ -0,0 +1,13 @@
+import { Env, env } from "../../../src/utils/env";
+
+describe("Unit", () => {
+  describe("Util", () => {
+    beforeAll(() => {
+      Env.warmup("env.test.json");
+    });
+    it.only("Should get env from env.json", () => {
+      const variable = env<string>("TEST_VAR", "error");
+      expect(variable).toBe("test");
+    });
+  });
+});
