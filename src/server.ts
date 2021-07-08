056c316954bb4d0b7b11b9242b067f053fcda43a
Lukas Jacomin <lukasjacomin@gmail.com>
2021-07-08 10:46:41 -0300

    new env implementation

--- src/server.ts
+++ src/server.ts
@@ -2,8 +2,11 @@ import { createExpressServer, useContainer } from "routing-controllers";
 import Container from "typedi";
 import controllers from "./controllers";
 import interceptors from "./interceptors";
+import { Env } from "./utils/env";
 
 useContainer(Container);
+
+Env.warmup();
 
 const server = createExpressServer({
   cors: true,
