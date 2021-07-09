2c59df450d04fcba0df53ab6814eca875005c0c3
Lukas Jacomin <lukasjacomin@gmail.com>
2021-07-09 01:56:10 -0300

    removed old env.json class init

--- src/server.ts
+++ src/server.ts
@@ -2,11 +2,8 @@ import { createExpressServer, useContainer } from "routing-controllers";
 import Container from "typedi";
 import controllers from "./controllers";
 import interceptors from "./interceptors";
-import { Env } from "./utils/env";
 
 useContainer(Container);
-
-Env.warmup();
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export async function createServer(): Promise<any> {
